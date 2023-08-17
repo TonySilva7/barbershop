b7119336d84ee55ef498a947e93bc3bdd613c929
TonySilva7 <tony.deve7oper@gmail.com>
2023-08-16 21:57:58 -0300

    feat: create user model

---
+++ lib/src/model/user_model.dart
@@ -0,0 +1,80 @@
+sealed class UserModel {
+  final int id;
+  final String name;
+  final String email;
+  final String? avatar;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.avatar,
+  });
+}
+
+class UserModelADM extends UserModel {
+  final List<String>? workDays;
+  final List<int>? workHours;
+
+  UserModelADM({
+    required super.id,
+    required super.name,
+    required super.email,
+    super.avatar,
+    this.workDays,
+    this.workHours,
+  });
+
+  factory UserModelADM.fromMap(Map<String, dynamic> json) {
+    return switch (json) {
+      {'id': final int id, 'name': final String name, 'email': final String email} => UserModelADM(
+          id: id,
+          name: name,
+          email: email,
+          avatar: json['avatar'],
+          workDays: json['workDays']?.cast<String>(),
+          workHours: json['workHours']?.cast<int>(),
+        ),
+      _ => throw ArgumentError('Invalid Json'),
+    };
+  }
+}
+
+class UserModelEmployee extends UserModel {
+  final int barberShopId;
+  final List<String> workDays;
+  final List<int> workHours;
+
+  UserModelEmployee({
+    required this.barberShopId,
+    required super.id,
+    required super.name,
+    required super.email,
+    required this.workDays,
+    required this.workHours,
+    super.avatar,
+  });
+
+  factory UserModelEmployee.fromMap(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': final int id,
+        'name': final String name,
+        'email': final String email,
+        'barbershop_id': final barberShopId,
+        'work_day': final List<String> workDays,
+        'work_hours': final List<int> workHours,
+      } =>
+        UserModelEmployee(
+          id: id,
+          name: name,
+          email: email,
+          barberShopId: barberShopId,
+          workDays: workDays.cast<String>(),
+          workHours: workHours.cast<int>(),
+          avatar: json['avatar'],
+        ),
+      _ => throw ArgumentError('Invalid Json'),
+    };
+  }
+}
